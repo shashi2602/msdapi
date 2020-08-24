8fd487e971fa15b6fa69b128f996e937de64d7af
shashi2602 <shashi.ppk@gmail.com>
2020-08-24 16:16:30 +0530

    settings.py change

--- msd/settings.py
+++ msd/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,11 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tx9od%9p*t^0e$px1elnr#ys(ti473l#@-s045bdfhy!vuz215'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["msdapi.herokuapp.com","127.0.0.1"]
 
-CORS_ORIGIN_ALLOW_ALL = True
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -140,4 +139,4 @@ REST_FRAMEWORK = {
 }
 
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
