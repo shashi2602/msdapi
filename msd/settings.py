a7cc7257bbc228f77fac3721179c43ec06b6802b
shashi2602 <shashi.ppk@gmail.com>
2020-08-24 16:21:36 +0530

    settings.py change

--- msd/settings.py
+++ msd/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'msd.urls'
@@ -123,6 +124,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
 
 STATIC_URL = '/static/'
 REST_FRAMEWORK = {
